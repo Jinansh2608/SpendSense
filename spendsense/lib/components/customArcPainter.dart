d019d882a3de0412f3bdcf6c8131e3ae0274e9ca
Yash-parmar1 <fireryleaf@gmail.com>
2025-07-28 20:34:37 +0530

    added animation

--- spendsense/lib/components/customArcPainter.dart
+++ spendsense/lib/components/customArcPainter.dart
@@ -3,72 +3,62 @@ import 'package:spendsense/constants/colors/colors.dart';
 import 'package:vector_math/vector_math.dart';
 
 class CustomArcPainter extends CustomPainter {
+
   final double start;
   final double end;
   final double width;
   final double blurWidth;
 
-  CustomArcPainter({
-    required this.start,
-    required this.end,
-    this.width = 16,
-    this.blurWidth = 6,
-  });
+  CustomArcPainter({this.start = 0, this.end = 180, this.width = 18, this.blurWidth = 6});
 
   @override
   void paint(Canvas canvas, Size size) {
-    const double maxDiameter = 320;
-    final double diameter = size.shortestSide < maxDiameter
-        ? size.shortestSide *2.68
-        : maxDiameter;
-    final double radius = diameter / 2;
+    var rect = Rect.fromCircle(
+        center: Offset(size.width / 2, size.height / 2),
+        radius: size.width / 2);
 
-    final Offset center = Offset(size.width / 2, size.height / 2);
-    final Rect rect = Rect.fromCircle(center: center, radius: radius);
+    var gradientColor = LinearGradient(
+        colors: [Ycolor.secondarycolor,  Ycolor.secondarycolor],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter);
 
-    final gradientColor = LinearGradient(
-      colors: [Ycolor.secondarycolor, Ycolor.secondarycolor],
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-    );
+    Paint activePaint = Paint()..shader = gradientColor.createShader(rect);
 
-    final Paint activePaint = Paint()
-      ..shader = gradientColor.createShader(rect)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = width
-      ..strokeCap = StrokeCap.round;
+    activePaint.style = PaintingStyle.stroke;
+    activePaint.strokeWidth = width;
+    activePaint.strokeCap = StrokeCap.round;
 
-    final Paint backgroundPaint = Paint()
-      ..color = Ycolor.gray60.withOpacity(0.5)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = width
-      ..strokeCap = StrokeCap.round;
+    Paint backgroundPaint = Paint();
+    backgroundPaint.color = Ycolor.gray60.withOpacity(0.5);
+    backgroundPaint.style = PaintingStyle.stroke;
+    backgroundPaint.strokeWidth = width;
+    backgroundPaint.strokeCap = StrokeCap.round;
 
-    final Paint shadowPaint = Paint()
-      ..color = Ycolor.secondarycolor.withOpacity(0.3)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = width + blurWidth
-      ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 5);
+    
 
-    final double startVal = 125.0 + start;
+    Paint shadowPaint = Paint()
+        ..color = Ycolor.secondarycolor.withOpacity(0.3) // .. is cascading used just after creting the object so basically paint.color would be ..color after making a paint object
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = width + blurWidth
+        ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 5);
 
-    // Draw base arc
-    canvas.drawArc(rect, radians(startVal), radians(290), false, backgroundPaint);
+    var startVal = 125.0 + start;
 
-    // Shadow arc
-    final Path shadowPath = Path();
-    shadowPath.addArc(rect, radians(startVal), radians(end));
-    canvas.drawPath(shadowPath, shadowPaint);
-
-    // Active arc
+    canvas.drawArc(rect, radians(startVal) , radians(290), false, backgroundPaint);
+    
+    //Draw Shadow Arc
+    Path path = Path();
+    path.addArc(rect, radians(startVal) , radians(end));
+    canvas.drawPath(path, shadowPaint );
+    
     canvas.drawArc(rect, radians(startVal), radians(end), false, activePaint);
   }
 
   @override
   bool shouldRepaint(covariant CustomArcPainter oldDelegate) {
-    return start != oldDelegate.start ||
-        end != oldDelegate.end ||
-        width != oldDelegate.width ||
-        blurWidth != oldDelegate.blurWidth;
+    return start != oldDelegate.start || end != oldDelegate.end;
   }
+
+  @override
+  bool shouldRebuildSemantics(CustomArcPainter oldDelegate) => false;
 }
