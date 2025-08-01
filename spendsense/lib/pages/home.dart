1f010d4a696cd76f96e587574dd1c8f15dc127a7
Yash-parmar1 <fireryleaf@gmail.com>
2025-08-01 20:23:52 +0530

    added the tiles and information panel

---
+++ spendsense/lib/pages/home.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:spendsense/pages/infoPanel.dart';
+
+class Transactiontile extends StatelessWidget {
+   final String tittttle;
+  const Transactiontile({super.key,required this.tittttle});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 20,left: 18),
+                      child: Text("data"),
+                    ),
+                    
+                  ),
+                  GestureDetector(
+                    onTap: () =>  Navigator.push(context, MaterialPageRoute(builder: (context)=>infoPanel())),
+                    child: SliverList(
+                      
+                      delegate: SliverChildBuilderDelegate((context,index){
+                        return Padding(
+                          padding: EdgeInsets.all(3),
+                          child: ListTile(
+                           
+                            title: Text(tittttle,
+                            style: TextStyle(
+                              fontSize: 20
+                            ),),
+                            subtitle: Text("world",style: TextStyle(
+                              fontSize: 15
+                            ),),
+                            trailing: Text("800",
+                            style: TextStyle(
+                              fontSize: 24
+                            ),),
+                          ),
+                        );
+                      },
+                      childCount: 20)
+                      ),
+                  ),
+                    SliverToBoxAdapter(
+                      child: Container(
+                        height: (MediaQuery.of(context).size.height*0.15),
+                       
+                      ),
+                    )
+                ],
+              );
+  }
+}
